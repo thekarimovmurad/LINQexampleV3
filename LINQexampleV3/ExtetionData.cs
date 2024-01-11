0d7b40c36d152c09c95d5f536a64a276a3b4294c
thekarimovmurad <thekarimovmurad@gmail.com>
2024-01-11 03:14:49 +0100

    Add project files.

---
+++ LINQexampleV3/ExtetionData.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexampleV3
+{
+    internal class ExtetionData
+    {
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public bool IsManager { get; set; }
+        public int DepartmentId { get; set; }
+    }
+    public class Department
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+    }
+    public static class Data
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Jones",
+                AnnualSalary = 60000.3m,
+                IsManager = true,
+                DepartmentId = 2
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 2,
+                FirstName = "Sarah",
+                LastName = "Jameson",
+                AnnualSalary = 80000.1m,
+                IsManager = true,
+                DepartmentId = 3
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "Douglas",
+                LastName = "Roberts",
+                AnnualSalary = 40000.2m,
+                IsManager = false,
+                DepartmentId = 1
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 4,
+                FirstName = "Jane",
+                LastName = "Stevens",
+                AnnualSalary = 30000.2m,
+                IsManager = false,
+                DepartmentId = 3
+            };
+            employees.Add(employee);
+
+            return employees;
+        }
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departments = new List<Department>();
+
+            Department department = new Department
+            {
+                Id = 1,
+                ShortName = "HR",
+                LongName = "Human Resources"
+            };
+            departments.Add(department);
+            department = new Department
+            {
+                Id = 2,
+                ShortName = "FN",
+                LongName = "Finance"
+            };
+            departments.Add(department);
+            department = new Department
+            {
+                Id = 3,
+                ShortName = "TE",
+                LongName = "Technology"
+            };
+            departments.Add(department);
+
+            return departments;
+        }
+
+        public static ArrayList GetHeterogeneousDataCollection()
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(100);
+            arrayList.Add("Bob Jones");
+            arrayList.Add(2000);
+            arrayList.Add(3000);
+            arrayList.Add("Bill Henderson");
+            arrayList.Add(new Employee { Id = 6, FirstName = "Jennifer", LastName = "Dale", AnnualSalary = 90000, IsManager = true, DepartmentId = 1 });
+            arrayList.Add(new Employee { Id = 7, FirstName = "Dane", LastName = "Hughes", AnnualSalary = 60000, IsManager = false, DepartmentId = 2 });
+            arrayList.Add(new Department { Id = 4, ShortName = "MKT", LongName = "Marketing" });
+            arrayList.Add(new Department { Id = 5, ShortName = "R&D", LongName = "Research and Development" });
+            arrayList.Add(new Department { Id = 6, ShortName = "PRD", LongName = "Production" });
+
+            return arrayList;
+        }
+
+    }
+}
---
+++ LINQexampleV3/Program.cs
@@ -0,0 +1,51 @@
+﻿using LINQexampleV3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+List<Employee> employeeList = Data.GetEmployees();
+List<Department> departmentList = Data.GetDepartments();
+
+#region Sorting Operation
+////method syntax
+//var result = employeeList.Join(departmentList, e => e.DepartmentId, d => d.Id,
+//    (emp, dep) => new
+//    {
+//        Id = emp.Id,
+//        FirstName = emp.FirstName,
+//        LastName = emp.LastName,
+//        AnnualSalary = emp.AnnualSalary,
+//        DepartmentId = emp.DepartmentId,
+//        DepartmentName = dep.LongName
+//    }).OrderBy(o => o.DepartmentId).ThenByDescending(o => o.AnnualSalary);
+//foreach (var item in result)
+//{
+//    Console.WriteLine($"First Name:{item.FirstName,-10} Last Name: {item.LastName,-10} Annual Salary: {item.AnnualSalary,10}\tDepartment Name: {item.DepartmentName}");
+//}
+
+////query syntax
+//var result = from emp in employeeList
+//             join dep in departmentList
+//             on emp.DepartmentId equals dep.Id
+//             orderby emp.DepartmentId, emp.AnnualSalary descending
+//             select new
+//             {
+//                Id= emp.Id,
+//                FirstName= emp.FirstName,
+//                LastName= emp.LastName,
+//                AnnualSalary = emp.AnnualSalary,
+//                DepartmentId= emp.DepartmentId,
+//                DepartmentName = dep.LongName
+//             };
+//foreach (var item in result)
+//{
+//    Console.WriteLine($"First Name:{item.FirstName,-10} Last Name: {item.LastName, -10} Annual Salary: {item.AnnualSalary,10}\tDepartment Name: {item.DepartmentName}");
+//}
+#endregion
+
+
+
+
+Console.ReadKey();
